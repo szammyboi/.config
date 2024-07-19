4cbcdf3ca3bd7e729bc894eb7c10d5d9891bd265
szammyboi <szammyboi@gmail.com>
2024-07-19 14:14:11 -0400

    added mason and lspconfig

--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -4,5 +4,16 @@ return {
 		opts = {},
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-	
+	{"williamboman/mason.nvim"},
+	{
+    'williamboman/mason-lspconfig',
+		config = function(self, opts)
+			require("mason").setup()
+			require("mason-lspconfig").setup {
+				ensure_installed = { "lua_ls", "clangd" },
+				automatic_installation = true
+			}
+		end
+	}, -- everything in this table is a spec 
+	{'neovim/nvim-lspconfig'},  -- same	
 }
