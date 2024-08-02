7e3f6986735cedf0ff4954efa25fb17df795b5db
szammyboi <78485053+szammyboi@users.noreply.github.com>
2024-08-02 18:54:05 -0400

    ColorSchemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,9 +5,25 @@ return {
 	    lazy = true,
 	    priority = 1000,
 	    init = function()
-	    	vim.cmd([[colorscheme catppuccin]])
+	    	--vim.cmd([[colorscheme catppuccin]])
 	    end,
 	},
+	{
+		"savq/melange-nvim",
+		lazy = true,
+		priority = 1000,
+		init = function()
+			--vim.cmd([[colorscheme melange]])
+		end
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+		priority = 1000,
+		init = function()
+			vim.cmd([[colorscheme kanagawa]])
+		end
+	}
 }
 
 
